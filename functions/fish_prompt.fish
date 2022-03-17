8e9c4b0dd11365e76e4000c84f23e499f2f1fd5b
Edoardo Ranghieri <edoardo@ranghieri.com>
2022-03-17 02:02:15 +0100

    tide -> shellder

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,38 +1,141 @@
+#
+# Segments functions
+#
+set -g current_bg NONE
+set -g segment_separator \uE0B0
+
+function prompt_segment -d "Function to draw a segment"
+	set -l bg
+	set -l fg
+	if [ -n "$argv[1]" ]
+		set bg $argv[1]
+	else
+		set bg normal
+	end
+	if [ -n "$argv[2]" ]
+		set fg $argv[2]
+	else
+		set fg normal
+	end
+	if [ "$current_bg" != 'NONE' -a "$argv[1]" != "$current_bg" ]
+		set_color -b $bg
+		set_color $current_bg
+		echo -n "$segment_separator "
+		set_color -b $bg
+		set_color $fg
+	else
+		set_color -b $bg
+		set_color $fg
+		echo -n " "
+	end
+	set current_bg $argv[1]
+	if [ -n "$argv[3]" ]
+		echo -n -s $argv[3] " "
+	end
+end
+
+function prompt_finish -d "Close open segments"
+	if [ -n $current_bg ]
+		set_color -b normal
+		set_color $current_bg
+		echo -n "$segment_separator "
+	end
+	set_color normal
+	set -g current_bg NONE
+end
+
+#
+# Components
+#
+function prompt_virtual_env -d "Display Python virtual environment"
+	if test "$VIRTUAL_ENV"
+		prompt_segment white black (basename $VIRTUAL_ENV)
+	end
+end
+
+function prompt_dir -d "Display the current directory"
+	prompt_segment 404040 FFFFFF (prompt_pwd)
+end
+
+function prompt_git -d "Display the current git state"
+	set -l ref
+	if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+		set ref (command git symbolic-ref HEAD 2> /dev/null)
+		if [ $status -gt 0 ]
+			set -l branch (command git show-ref --head -s --abbrev |head -n1 2> /dev/null)
+			set ref "➦ $branch "
+		end
+		set branch_symbol \uE0A0
+		set -l branch (echo $ref | sed  "s-refs/heads/-$branch_symbol -")
+
+		set -l BG PROMPT
+		set -l dirty (command git status --porcelain --ignore-submodules=dirty 2> /dev/null)
+		if [ "$dirty" = "" ]
+			set BG green
+			set PROMPT "$branch"
+		else
+			set BG yellow
+			set dirty ''
+
+			# Check if there's any commit in the repo
+			set -l empty 0
+			git rev-parse --quiet --verify HEAD > /dev/null 2>&1; or set empty 1
+
+			set -l target
+			if [ $empty = 1 ]
+				# The repo is empty
+				set target '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
+			else
+				# The repo is not emtpy
+				set target 'HEAD'
+
+				# Check for unstaged change only when the repo is not empty
+				set -l unstaged 0
+				git diff --no-ext-diff --ignore-submodules=dirty --quiet --exit-code; or set unstaged 1
+				if [ $unstaged = 1 ]; set dirty $dirty'●'; end
+			end
+
+			# Check for staged change
+			set -l staged 0
+			git diff-index --cached --quiet --exit-code --ignore-submodules=dirty $target; or set staged 1
+			if [ $staged = 1 ]; set dirty $dirty'✚'; end
+
+			# Check for dirty
+			if [ "$dirty" = "" ]
+				set PROMPT "$branch"
+			else
+				set PROMPT "$branch $dirty"
+			end
+		end
+		prompt_segment $BG black $PROMPT
+	end
+end
+
+function prompt_status -d "The symbols for a non zero exit status, root and background jobs"
+		if [ $RETVAL -ne 0 ]
+			prompt_segment white red $RETVAL
+		end
+
+		# if superuser (uid == 0)
+		set -l uid (id -u $USER)
+		if [ $uid -eq 0 ]
+			prompt_segment black yellow "⚡"
+		end
+
+		# Jobs display
+		if [ (jobs -l | wc -l) -gt 0 ]
+			prompt_segment black cyan "⚙"
+		end
+end
+
+#
+# Prompt
+#
 function fish_prompt
-end
-
-status is-interactive || exit
-
-_tide_remove_unusable_items
-
-# The first element in $$_tide_prompt_var is right prompt
-# All remaining ones are 'left' prompt (also upper right in 2-line prompts)
-set -g _tide_prompt_var _tide_prompt_$fish_pid
-
-function _tide_refresh_prompt --on-variable $_tide_prompt_var
-    set -g _tide_self_repainting # prevents us from creating a second background job
-    commandline --function repaint
-end
-
-function fish_prompt
-    _tide_last_status=$status _tide_last_pipestatus=$pipestatus if not set -e _tide_self_repainting
-        jobs --query
-        fish --command "_tide_jobs_status=$status CMD_DURATION=$CMD_DURATION COLUMNS=$COLUMNS \
-            fish_bind_mode=$fish_bind_mode set -U $_tide_prompt_var (_tide_prompt)" &
-        builtin disown
-
-        command kill $_tide_last_pid 2>/dev/null
-        set -g _tide_last_pid $last_pid
-    end
-
-    test "$tide_prompt_add_newline_before" = true && echo
-    string unescape $$_tide_prompt_var[1][2..]
-end
-
-function fish_right_prompt
-    string unescape $$_tide_prompt_var[1][1]
-end
-
-function _tide_on_fish_exit --on-event fish_exit
-    set -e $_tide_prompt_var
+	set -g RETVAL $status
+	prompt_status
+	prompt_virtual_env
+	prompt_dir
+	command -v git &>/dev/null; and prompt_git
+	prompt_finish
 end
