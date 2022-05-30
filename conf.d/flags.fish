b27460bea4e660aadcb6cb83364102db89c6874c
Edoardo Ranghieri <edoardo@ranghieri.com>
2022-05-30 22:11:14 +0200

    fix paths

--- conf.d/flags.fish
+++
@@ -1,2 +0,0 @@
-set -gx LDFLAGS "-L/opt/homebrew/opt/node@16/lib -L/opt/homebrew/opt/mysql-client/lib"
-set -gx CPPFLAGS "-I/opt/homebrew/opt/node@16/include -I/opt/homebrew/opt/mysql-client/include"
--- config.fish
+++ config.fish
@@ -1,2 +1,7 @@
 if status is-interactive
 end
+
+set -gx LDFLAGS "-L/opt/homebrew/opt/node@16/lib -L/opt/homebrew/opt/mysql-client/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/node@16/include -I/opt/homebrew/opt/mysql-client/include"
+
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/mysql-client/lib/pkgconfig"
