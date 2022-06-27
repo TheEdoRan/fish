ea665b946996363ce78ff91296f9788be6e853bf
Edoardo Ranghieri <edoardo@ranghieri.com>
2022-06-27 02:24:05 +0200

    add `secret` abbrev, update path vars

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
 end
 
-set -gx LDFLAGS "-L/opt/homebrew/opt/node@16/lib -L/opt/homebrew/opt/mysql-client/lib"
-set -gx CPPFLAGS "-I/opt/homebrew/opt/node@16/include -I/opt/homebrew/opt/mysql-client/include"
+set -gx LDFLAGS "-L/opt/homebrew/opt/node@16/lib -L/opt/homebrew/opt/libpq/lib -L/opt/homebrew/opt/mysql-client/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/node@16/include -I/opt/homebrew/opt/libpq/include -I/opt/homebrew/opt/mysql-client/include"
 
-set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/mysql-client/lib/pkgconfig"
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/libpq/lib/pkgconfig:/opt/homebrew/opt/mysql-client/lib/pkgconfig"
